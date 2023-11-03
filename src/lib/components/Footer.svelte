fee5706f09e03f3990a549e7864ab9be0d0c094d
Pyroarsonist <velikiydan@gmail.com>
2023-11-03 17:54:24 +0200

    added build version

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,15 +1,22 @@
 <script>
-	import LucideCode2 from '~icons/lucide/code-2';
+    import LucideCode2 from '~icons/lucide/code-2';
+    import { version } from '$app/environment';
+
 </script>
 
-<div class="mt-20 items-center text-xs text-stone-400">
-	<a
-		href="https://github.com/Pyroarsonist/dnd5e-ua-ref"
-		target="_blank"
-		rel="noreferrer"
-		class="flex items-center justify-center"
-	>
-		<LucideCode2 class="h-5 w-5" />
-		<span class="ml-1 font-medium hover:underline">open source by Pyroarsonist</span>
-	</a>
+<div class="mt-20 items-center text-xs text-stone-400 flex flex-row items-center justify-center">
+    <div class="ml-auto"></div>
+    <a
+            href="https://github.com/Pyroarsonist/dnd5e-ua-ref"
+            target="_blank"
+            rel="noreferrer"
+            class="flex items-center justify-center"
+    >
+        <LucideCode2 class="h-5 w-5"/>
+        <span class="ml-1 font-medium hover:underline">open source by Pyroarsonist</span>
+    </a>
+    <div class="ml-auto"></div>
+    <div>
+        v{version}
+    </div>
 </div>
